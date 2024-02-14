45a71718126acb829f4a90e1fa6ca060e877949f
Lina Vela <u20232217336@usco.edu.co>
2024-02-13 20:18:46 -0500

    Mi primer hola mundo

---
+++ PrimeraApp.java
@@ -0,0 +1,17 @@
+
+package primeraapp;
+
+/**
+ *
+ * @author LINA MARGARITA VELA BONILLA 20232217336
+ */
+public class PrimeraApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!");
+    }
+    
+}
